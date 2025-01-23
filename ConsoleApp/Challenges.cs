a9c191c6fe8070affc2d7f9f6a82b1e9d66daf24
BEEKRUM <bikram.duwal@amniltech.com>
2025-01-23 15:40:15 +0545

    Add project files.

---
+++ ConsoleApp/Challenges.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp
+{
+    public static class Challenges
+    {
+
+        //methods
+        public static void Sum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"Sum of {a} and {b} is: " + result);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using ConsoleApp;
+
+Console.WriteLine("First Number = ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Second Number = ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+
+Challenges.Sum(firstNumber, secondNumber); // Sum of 1 and 2 is: 3
