c13fc7a9062e288aea13e92b890162704b4eb8a2
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-24 14:58:15 +0545

    Initial commit

---
+++ Task1/Task1/Details.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class Details
+    {
+        public static void UserDetails()
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your address: ");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("--- User Information ---");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Address: {address}");
+        }
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task1;
+
+
+while(true)
+{
+    Console.WriteLine("1. Enter User Details");
+    Console.WriteLine("2. Exit");
+    Console.Write("Choose an option: ");
+    string choice = Console.ReadLine();
+
+    if (choice == "1")
+    {
+        Details.UserDetails();
+    }
+    else if (choice == "2")
+    {
+        Console.WriteLine("Goodbye!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice. Please try again.");
+    }
+}
