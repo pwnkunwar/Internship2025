0ea8b95c1c7eb4b865e448e0e663c5fba1a823bb
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-25 10:30:11 +0545

    added ArrayOfMultiples class

---
+++ ConsoleApp/ArrayOfMultiples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class ArrayOfMultiples
+    {
+        // Create a function that takes two numbers as arguments (num, length) and
+        // returns an array of multiples of num until the array length reaches length. 
+        public static int[] ArrayOfMultiple(int num, int length)
+        {
+            int[] array = new int[length];
+            for(int i=1; i <= length; i++)
+            {
+                array[i-1] = num * i;
+            }
+            return array;
+        }
+    }
+}
