3247ded54d966f2ae8ad01471f9c02fd7ce0ae0d
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-25 10:51:24 +0545

    Add function to reverse integer and append original

---
+++ ConsoleApp/ReverseAndNot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class ReverseAndNot
+    {
+        public static string Reverse(int i)
+        {
+            //Write a function that takes an integer i and returns a string
+            //with the integer backwards followed by the original integer.
+            char[] chars = i.ToString().ToCharArray();
+            char[] reverse = new char[chars.Length];
+            int index = chars.Length;
+            for(int j = 0; j < chars.Length; j++)
+            {
+                reverse[j] = chars[index -1];
+                index--;
+            }
+            string reverseAndNot = new string(reverse) + i.ToString();
+            return reverseAndNot;
+        }
+    }
+}
