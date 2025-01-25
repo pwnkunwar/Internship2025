2fc1317f84985aaa8afc6a9f3db3e6fdd71c8491
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-25 11:13:26 +0545

    Integrate another static class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,10 +4,23 @@
 
 using ConsoleApp;
 
-Console.WriteLine("First Number = ");
+/*Console.WriteLine("First Number = ");
 int firstNumber = int.Parse(Console.ReadLine());
 Console.WriteLine("Second Number = ");
-int secondNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());*/
 
 
-Challenges.Sum(firstNumber, secondNumber); // Sum of 1 and 2 is: 3
+/*Challenges.Sum(firstNumber, secondNumber);*/ // Sum of 1 and 2 is: 3
+
+
+Console.WriteLine(SumOfTwoNumber.Sum(12, 10));
+
+int[] array = ArrayOfMultiples.ArrayOfMultiple(6, 5);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+Console.WriteLine(ReverseAndNot.Reverse(123));
+
+Console.WriteLine(ATMpinCodeValidation.PinValidation("111111"));
