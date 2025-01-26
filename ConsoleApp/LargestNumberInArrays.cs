2258aff2f05a37ce7bf5d0b4b6af3b3d584cf2f9
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-26 13:25:08 +0545

    added LargestNumberInArrays class

---
+++ ConsoleApp/LargestNumberInArrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class LargestNumberInArrays
+    {
+        //Create a function that takes an array of arrays with numbers.
+        //Return a new (single) array with the largest numbers of each.
+        public static double[] FindLargest(double[][] values)
+        {
+            double[] largestArray = new double[values.Length];
+            int index = 0;
+
+            foreach (double[] array in values) 
+            {
+                double largest = double.MinValue; 
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j] > largest)
+                    {
+                        largest = array[j];
+                    }
+                }
+                largestArray[index] = largest;
+                index++;
+            }
+
+            return largestArray; 
+        }
+    }
+}
