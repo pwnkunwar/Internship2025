9b53a503c0afc13e6ee419e4293a80e57ba655d3
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-27 21:32:18 +0545

    call static ListExample class from Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,7 +12,7 @@ int secondNumber = int.Parse(Console.ReadLine());*/
 
 /*Challenges.Sum(firstNumber, secondNumber);*/ // Sum of 1 and 2 is: 3
 
-
+/*
 Console.WriteLine(SumOfTwoNumber.Sum(12, 10));
 
 int[] array = ArrayOfMultiples.ArrayOfMultiple(6, 5);
@@ -30,4 +30,13 @@ Console.WriteLine(ATMpinCodeValidation.PinValidation("111111"));
 for (int j = 0; j < largest.Length; j++)
 {
     Console.WriteLine($"Largest: {largest[j]}");
-}
+}*/
+
+
+ListExample.ListDemo();
+ListExample.Insert();
+ListExample.Remove();
+ListExample.Clear();
+ListExample.ListToArray();
+ListExample.ArrayToList();
+ListExample.LINQ();
