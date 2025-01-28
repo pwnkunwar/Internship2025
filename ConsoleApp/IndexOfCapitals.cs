6ed258cf51d4dafa675be89299c38bf69076ecce
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-28 19:16:45 +0545

    Add method to find capital letter indices

---
+++ ConsoleApp/IndexOfCapitals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp
+{
+    public static class IndexOfCapitals
+    {
+      //  Create a function that takes a single string as argument
+       //     and returns an ordered array containing the indices of all capital letters in the string.
+        public static int[] IndexOfCapitalsLetters(string str)
+        {
+            char[] characters = str.ToCharArray();
+            char[] capitalLetters = new char[26];
+            List<int> indexOfCapitals = new List<int>();
+            for(int i = 0;  i < capitalLetters.Length; i++)
+            {
+                capitalLetters[i] = (char)('A' + i);
+            }
+            
+            for(int j=0; j < characters.Length; j++)
+            {
+                for (int k = 0; k < capitalLetters.Length; k++)
+                {
+                    if (characters[j] == capitalLetters[k])
+                    {
+                        indexOfCapitals.Add(j);
+                    }
+                }
+            }
+            return indexOfCapitals.ToArray();
+
+        }
+    }
+}
