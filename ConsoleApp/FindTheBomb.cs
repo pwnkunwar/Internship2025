2b1bf16e876fcf32025bb7ce19332803b98b6378
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-28 19:35:52 +0545

    Add Bomb Detection Function with Case-Insensitive Search

---
+++ ConsoleApp/FindTheBomb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class FindTheBomb
+    {
+        //Create a function that finds the word "bomb" in the given string (not case sensitive). If found,
+        //return "Duck!!!", otherwise, return "There is no bomb, relax.".
+        public static string Bomb(string txt)
+        {
+            if(txt.ToLower().Contains("bomb"))
+            {
+                return "Duck!!!";
+            }
+            return "There is no bomb, relax.";
+        }
+    }
+}
