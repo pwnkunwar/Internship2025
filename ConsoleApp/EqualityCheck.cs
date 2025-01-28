6caf3994ed0458ffe1f71d1fcb74af559fb33425
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-28 18:50:22 +0545

    Add function to check equality of value and type for two objects

---
+++ ConsoleApp/EqualityCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class EqualityCheck
+    {
+        //this function take two paramter and check whethere the value and value type are equal or not
+        public static bool CheckEquality(object a, object b)
+        {
+
+            if (a.GetType() == b.GetType())
+            {
+                if (a.Equals(b))
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
