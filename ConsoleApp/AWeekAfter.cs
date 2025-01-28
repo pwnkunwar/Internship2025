4172f35e8a2834f0de9c2d05ae4c8dcdcb215729
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-28 20:49:47 +0545

    Program to return date by adding 1 week

---
+++ ConsoleApp/AWeekAfter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class AWeekAfter
+    {
+        //this method add 7 days to a date and return a date
+        public static string WeekAfter(string date)
+        {
+            DateTime dateTime = DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            return dateTime.AddDays(7).ToString("dd/MM/yyyy");
+        }
+    }
+}
