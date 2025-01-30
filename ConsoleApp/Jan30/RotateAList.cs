e621128c5bc61819922277f96600cc28bc1b64bb
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-30 16:34:41 +0545

    Jan30 exercises

---
+++ ConsoleApp/Jan30/RotateAList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Jan30
+{
+    public static class RotateAList
+    {
+        public static void Rotate()
+        {
+            List<int> RotatedList = new List<int>();
+            List<int> numbers = new List<int>{ 1, 2, 3, 4, 5 };
+            Console.WriteLine("Enter the rotation positions");
+            int k = int.Parse(Console.ReadLine());
+            for(int i=k; i< numbers.Count; i++)
+            { 
+                RotatedList.Add(numbers[i]);
+            }
+            for(int i = 0; i< k; i++)
+            {
+                RotatedList.Add(numbers[i]);
+            }
+            Console.WriteLine($"Merge List: {string.Join(", ", RotatedList)}");
+
+        }
+    }
+}
---
+++ ConsoleApp/Jan30/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Jan30;
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+    public Student(string name, int age, string course)
+    {
+        Name = name;
+        Age = age;
+        Course = course;
+    }
+    public void PrintDetails()
+    {
+      Console.WriteLine($"Student name is {Name}, age is {Age}, and Course is {Course}");
+    }
+}
+
+
+
+
