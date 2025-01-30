d2c18682fa067b496f170938f10190cf9eca3073
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-30 22:53:56 +0545

    Basic Inheritance class added

---
+++ ConsoleApp/Jan30/BasicInheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Jan30
+{
+    public  class Vehicle
+    {
+        public  string Brand {  get; set; }
+
+        public  void Start()
+        {
+            Console.WriteLine($"{Brand} is starting ...");
+        }
+    }
+
+    public  class Car : Vehicle
+    {
+        public int Wheels { get; set; } = 4;
+
+        public void Drive()
+        {
+            Console.WriteLine($"{Brand} is driving with {Wheels} wheels");
+        }
+    }
+
+
+}
