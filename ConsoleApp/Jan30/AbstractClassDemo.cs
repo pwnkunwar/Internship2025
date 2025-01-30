14889f06868231ad6fe397b2681a541f287af56c
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-30 23:05:40 +0545

    Exmample of Abstract class added

---
+++ ConsoleApp/Jan30/AbstractClassDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Jan30
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public abstract void MakeSound();
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} is eating.");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog barks: Woof woof!");
+        }
+    }
+}
+
