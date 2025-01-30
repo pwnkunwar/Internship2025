3153a48ddff9ef1b3efe332f3453017db19473c8
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-30 22:59:29 +0545

    interface example added

---
+++ ConsoleApp/Jan30/InterfaceExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Jan30;
+    public interface IVehicle
+    {
+        void Start();
+        void Stop();
+    }
+    public class MotorVehicle : IVehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("Car is starting...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Car is stopping...");
+        }
+    }
+
