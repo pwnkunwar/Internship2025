1e594f05e69d199748f89228fe88b02ac99348d8
pwn1kunwar <pwn1kunwar@gmail.com>
2025-01-30 22:54:27 +0545

    Multilevel Inhertiance added

---
+++ ConsoleApp/Jan30/MultilevelInheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Jan30
+{
+    //base class
+    public class Person
+    {
+        public string Name { get; set; }    
+        public void ShowName()
+        {
+            Console.WriteLine($"Name:{Name}");
+        }
+    }
+
+    //Derived Class
+    public class Employee : Person
+    {
+        public double Salary { get; set; }
+        public void ShowSalary()
+        {
+            Console.WriteLine($"{Name} Salary : {Salary}");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public string Department { get; set; }
+        public void ShowDepartment()
+        {
+            Console.WriteLine($"{Name} manges the {Department} department");
+        }
+
+    }
+}
