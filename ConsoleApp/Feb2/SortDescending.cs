338c3b3eab3e1df448781c2f994460bc616f3179
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-03 12:39:10 +0545

    soring the number in descending order

---
+++ ConsoleApp/Feb2/SortDescending.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb2
+{
+    public static class SortDescending
+    {
+        public static int SortDescendingNumber(int num)
+        {
+            char[] chars = num.ToString().ToCharArray();
+            char[] descendingOrder = new char[chars.Length];
+            for(int i=0; i<chars.Length; i++)
+            {
+                descendingOrder[i] = chars[chars.Length -1- i];
+            }
+            return int.Parse(new string(descendingOrder));
+        }
+    }
+}
