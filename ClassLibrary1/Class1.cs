fac36583f53075c6a14fbc0eb8154b25736017d2
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-05 14:59:09 +0545

    ClassLibrary1 added to demostrate internal access modifiers

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary1
+{
+    internal class InternalClass
+    {
+        public string GetMessage()
+        {
+            return "Hello from Internal Class!";
+        }
+    }
+    public class PublicClass
+    {
+        public void GetPublicMessage()
+        {
+            Console.WriteLine("Hello from Public Class!");
+        }
+    }
+
+}
