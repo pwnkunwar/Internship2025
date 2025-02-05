018c66ada7de359c7f240c54a478eb293bf473a0
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-05 15:51:39 +0545

    Program to read content from file

---
+++ ConsoleApp/Feb4/FileHandlingRead.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb4
+{
+    public class FileHandlingRead
+    {
+        public void ReadFromFile(string filePath)
+        {
+            try
+            {
+                // check if the file exists 
+                if(File.Exists(filePath))
+                {
+                    using(StreamReader reader = new StreamReader(filePath))
+                    {
+                        string content = reader.ReadToEnd();  // read all content
+                        Console.WriteLine("File Content");
+                        Console.WriteLine(content);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The file does not exists.");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
