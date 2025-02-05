9547535e2687598c1935bd8ba1c79c40710d5709
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-05 15:34:37 +0545

    Add file handling functionality: Write to file

---
+++ ConsoleApp/Feb4/FileHandlingWrite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb4
+{
+    public class FileHandling
+    {
+        public void WriteToFile(string filePath, string content)
+        {
+            try
+            {
+                // write content to the file
+                using(StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.Write(content);
+                    Console.WriteLine("Content written to file sucessfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+    }
+}
