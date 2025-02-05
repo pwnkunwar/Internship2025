dbfffd80c53fc4ac577e62bda36ad785659b9c63
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-05 15:23:36 +0545

    added the demo of ProtectedPrivate accessifiers

---
+++ ConsoleApp/Feb4/ProtectedPrivateDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb4
+{
+    public class BaseClass1
+    {
+        private protected int secretNumber = 42;
+        public void ShowSecret()
+        {
+            Console.WriteLine($"Secret number: {secretNumber}");
+        }
+    }
+    public class DerivedClass1 : BaseClass1
+    {
+        public void DisplaySecret()
+        {
+            Console.WriteLine($"Accessing private protected: {secretNumber}");
+        }
+    }
+}
