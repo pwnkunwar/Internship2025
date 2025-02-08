c7d46b3f55f1e0d5a28d038c4c57d498bd5ac344
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-08 09:59:14 +0545

    Feb7 work added

---
+++ ConsoleApp/Feb7/baseKeywordDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb7
+{
+    // calling base class constructor
+    public class Animal2
+    {
+        public string Name;
+
+        public Animal2(string name)
+        {
+            Name = name;
+            Console.WriteLine($"Animal constructor called");
+        }
+    }
+    public class Dog2 : Animal2
+    {
+        public Dog2(string name): base(name)
+        {
+            Console.WriteLine($"Dog constructor called");
+        }
+    }
+}
