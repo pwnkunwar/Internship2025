e40af4ba927a8ddb15a2bc53342d2a5d2daa6721
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-08 09:58:36 +0545

    Frb6 work puhsed

---
+++ ConsoleApp/Feb6/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb6
+{
+    public class Person1
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public virtual void Info()
+        {
+            Console.WriteLine("This is Person class Info method");
+        }
+    }
+    public class Employee1 : Person1
+    {
+        public void Salary()
+        {
+            Console.WriteLine("Employee Salary");
+        }
+        public override void Info()
+        {
+            Console.WriteLine("This is Employee1 class Info method");
+
+        }
+    }
+    public class Manage : Employee1
+    {
+        public void Bonus()
+        {
+            Console.WriteLine("Bonus Manage");
+        }
+        public override void Info()
+        {
+            Console.WriteLine("This is Manage class Info method");
+
+        }
+    }
+}
