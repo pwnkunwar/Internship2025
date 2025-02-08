56ee0c88a8eff6eddbd6ba880d2ada4c59b1a647
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-08 10:15:43 +0545

    demo of base keyword accesing Base class properties

---
+++ ConsoleApp/Feb7/AccessingBaseClassProperties.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb7
+{
+    public class Device
+    {
+        public virtual string Model { get; set; } = "Generic Device";
+    }
+    public class Smartphone : Device
+    {
+        public override string Model
+        {
+            get
+            {
+                return base.Model + "This is a Smartphone";
+            }
+        }
+    }
+}
