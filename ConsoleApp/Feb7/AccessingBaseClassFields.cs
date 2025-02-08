6db9db334d9563e18b337c97784f4909f2a07a04
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-08 10:15:57 +0545

    demo of base keyword accesing Base class fields

---
+++ ConsoleApp/Feb7/AccessingBaseClassFields.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb7
+{
+    public class Building
+    {
+        public string Structure = "Concrete";
+    }
+    public class House : Building
+    {
+        public string Structure = "Residential";
+        public void ShowStructure()
+        {
+            Console.WriteLine("Base Structure: "+ base.Structure);
+            Console.WriteLine("Derived Structure: "+ Structure);
+        }
+    }
+}
