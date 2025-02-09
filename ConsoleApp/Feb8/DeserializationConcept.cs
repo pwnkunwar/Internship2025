9101b0e0f2041eddc5f6315274719afbd25c11e4
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-09 14:49:22 +0545

    Deserialization Example added

---
+++ ConsoleApp/Feb8/DeserializationConcept.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb8
+{
+    public class Product8
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public void JsonDeserialization()
+        {
+            string jsonString = "{\"Name\":\"Laptop\", \"Price\":1200.50}";
+            Product8 product= JsonSerializer.Deserialize<Product8>(jsonString);
+            Console.WriteLine($"Product Name:{product.Name}, Price: {product.Price}");
+        }
+    }
+}
