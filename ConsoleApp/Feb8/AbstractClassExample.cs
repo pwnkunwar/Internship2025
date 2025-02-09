85e3052cd98f09b7e4ab0055c2dc4703e2bac07b
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-09 14:38:24 +0545

    Example of abstract class added

---
+++ ConsoleApp/Feb8/AbstractClassExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb8
+{
+    public abstract class Appliance
+    {
+        public string Brand { get; set; }
+        public Appliance(string brand)
+        {
+            Brand = brand;
+        }
+
+        // Abstract method (must be implemented in derived classes)
+        public abstract void TurnOn();
+
+        // Non-abstract method
+        public void DisplayBrand()
+        {
+            Console.WriteLine($"Applicance Brand: {Brand}");
+        }
+    }
+    // Derived class WashingMachine
+    public class WashingMachine : Appliance
+    {
+        public WashingMachine(string brand): base(brand) 
+        {
+            
+        }
+        public override void TurnOn()
+        {
+            Console.WriteLine($"{Brand} Washing Mahine is now running!");
+        }
+    }
+}
