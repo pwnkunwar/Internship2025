c3532cd9701a54b5b22322c2034ac16b82fb0cd9
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-09 14:31:01 +0545

    Feb8 work added

---
+++ ConsoleApp/Feb8/StudentManagementSystem.cs
@@ -0,0 +1,45 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb8
+{
+    public class Student10
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+        public Student10(string name, int age, double grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Student Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Grade: {Grade}");
+
+        }
+    }
+    public class GraduateStudent : Student10
+    {
+        public string ResearchTopic { get; set; }
+        public GraduateStudent(string name, int age, double grade, string researchTopic)
+            : base(name, age, grade)
+        {
+            ResearchTopic = researchTopic;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Research Topic {ResearchTopic}");
+        }
+
+    }
+    }
