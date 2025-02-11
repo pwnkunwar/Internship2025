bd75e0db110eed68de11b4e9f059852a610818ee
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-11 13:52:04 +0545

    Program that will find the number of vowels in a string

---
+++ ConsoleApp/Feb10/CountVowels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb10
+{
+    public static class CountVowels
+    {
+        public static void VowelsCount(string input)
+        {
+            string vowels = "AEIOUaeiou";
+            Console.WriteLine(input.Count(c=> vowels.Contains(c)));
+        }
+    }
+}
