70a69e678c939e8eb20def5ff94b34467b437857
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-11 13:43:04 +0545

    Program that return the total number of letters and digits

---
+++ ConsoleApp/Feb10/CountAll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb10
+{
+    public static class CountAll
+    {
+        public static void count_all(string input)
+        {
+            var result = new
+            {
+                LETTERS = input.Count(char.IsLetter),
+                DIGITS = input.Count(char.IsDigit),
+            };
+            Console.WriteLine($"LETTERS = {result.LETTERS}, DIGITS = {result.DIGITS}");
+        }
+    }
+}
