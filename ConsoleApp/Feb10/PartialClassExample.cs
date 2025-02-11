a051018ab84143966468e44b691a2fee45f8f686
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-11 13:14:48 +0545

    Feb10 work added

---
+++ ConsoleApp/Feb10/PartialClassExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb10
+{
+    partial class Person10
+    {
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }
+    }
+}
---
+++ ConsoleApp/Feb10/PartialClassExample1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb10
+{
+    partial class Person10
+    {
+        public void ShowFullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+    }
+}
