6b035160f2e9398b2ccb572c03b07f9e4b2b5b8f
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-13 14:14:28 +0545

    Feb12 work added

---
+++ ConsoleApp/Feb12/LINQExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb12
+{
+    public class LINQExample
+    {
+        public void LINQEx()
+        {
+            List<int> numbers = new List<int> { 1,2,3,4,5,6,7,8,9,10};
+
+            //Query Syntax
+            var evenNumbers = from num in numbers
+                              where num % 2 ==0
+                              select num;
+
+            // Method syntax
+            var evenNumbersMethod = numbers.Where(num => num % 2 == 0);
+            Console.WriteLine("Even numbers {Query Synatx}");
+            foreach(var num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Even numbers {Method Syntax}");
+            foreach(var num in evenNumbersMethod)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
