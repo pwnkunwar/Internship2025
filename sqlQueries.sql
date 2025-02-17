9678b25999af9a75911f6db3591ed53c2958ca66
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-17 15:04:51 +0545

    added sql queries

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -144,6 +144,8 @@ EXEC GetEmployeeById @UserId = 1;
 -- Stored Procedure for Inserting Data
 
 
+
+
 CREATE PROCEDURE InserEmployee
 @FirstName NVARCHAR(50),
 @LastName NVARCHAR(50),
@@ -177,3 +179,76 @@ EXEC CheckUserExists @EmployeeId =2;
 -- DELETE PROCEDURE
 
 DROP PROCEDURE CheckUserExists;
+
+
+
+/* https://www.sql-practice.com practise  */
+SELECT * FROM patients
+
+
+select first_name,last_name,gender FROM patients WHERE gender='M';
+
+select first_name,last_name FROM patients where allergies IS NULL;
+
+select first_name from patients where first_name LIKE 'C%'; -- first name started with C letter 
+
+select first_name, last_name FROm patients where weight between 100 AND 120;
+
+
+
+update patients SET allergies='NKA'   where allergies is null;
+
+select concat(first_name,' ', last_name) AS full_name from patients;
+
+
+
+SELECT count(*) AS a FROm admissions;
+
+
+SELECT 
+    p.FirstName, 
+    p.LastName, 
+    prov.ProvinceName
+FROM Patients p
+JOIN Provinces prov ON p.ProvinceCode = prov.ProvinceCode;
+
+select count(*) FROm patients where year(birth_date) = 2010;
+
+
+
+
+select first_name,last_name, height from patients where height= (select max(height) from patients);
+
+ 
+
+
+select * FROM patients where patient_id in (1,45,534,879,1000);
+
+select * from admissions where admission_date = discharge_date;
+
+
+
+select patient_id, count(*) attending_doctor_id from admissions where patient_id = 579;
+
+
+
+select distinct(city) As unique_cities from patients where province_id = 'NS';
+
+
+select first_name, last_name, birth_date from patients where height > 160 and weight > 70;
+
+
+select first_name, last_name , allergies from patients where allergies Is not NULL and city = 'Hamilton';
+
+
+
+select distinct year(birth_date) from patients order by  birth_date asc;
+
+select first_name from patients group by first_name having count(*) =1;
+
+
+select patient_id, first_name from patients where first_name like 's%s' And len(first_name) >= 6;
+
+
+
+select p.patient_id, p.first_name, p.last_name From patients p join admissions ad ON p.patient_id = ad.patient_id where ad.diagnosis = 'Dementia';
