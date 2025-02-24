def75f45f4101ba71d02a35df206cd752c1e360d
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-24 10:07:18 +0545

    Feb23 work added

---
+++ ConsoleApp/Feb23/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb23
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-3SS2B66;Initial Catalog=ProductApp;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ ConsoleApp/Feb23/CRUDEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Feb23
+{
+    public class CRUDEntity
+    {
+        public void Execute()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nSelect an option:");
+                Console.WriteLine("1.Create Product");
+                Console.WriteLine("2.Read Products");
+            }
+        }
+    }
+}
