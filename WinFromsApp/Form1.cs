890f84f873a97993a1a6b2ae1b3cf9bcef2b3734
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-25 12:21:46 +0545

    Simple WinForms App

---
+++ WinFromsApp/Form1.cs
@@ -0,0 +1,26 @@
+namespace WinFromsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = textBox1.Text;
+            MessageBox.Show("You entered: "+ text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
