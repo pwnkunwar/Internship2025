421cf3937c9d6b6c2bd8fef9b3c00752497ddb6e
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-27 12:06:48 +0545

    Initialize .NET 8.0 Windows Forms application
    Updated ContactUsForm.csproj to define the project as a .NET 8.0 Windows Forms application. Added Form1.Designer.cs for the layout and properties of controls, Form1.cs for the Form1 class and event handlers, Form1.resx for resource definitions, and Program.cs with the Main method to run the application.

---
+++ ContactUsForm/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ContactUsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(157, 32);
+            label1.TabIndex = 0;
+            label1.Text = "CONTACT US";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(463, 20);
+            label2.TabIndex = 1;
+            label2.Text = "We'd love to hear from you, please drop us a line if you've any query.";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 104);
+            label3.Name = "label3";
+            label3.Size = new Size(88, 21);
+            label3.TabIndex = 2;
+            label3.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 138);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(261, 34);
+            textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(313, 104);
+            label4.Name = "label4";
+            label4.Size = new Size(86, 21);
+            label4.TabIndex = 4;
+            label4.Text = "Last Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(313, 138);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(261, 34);
+            textBox2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(12, 197);
+            label5.Name = "label5";
+            label5.Size = new Size(112, 21);
+            label5.TabIndex = 6;
+            label5.Text = "Email Address";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 232);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(562, 34);
+            textBox3.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(12, 287);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 21);
+            label6.TabIndex = 8;
+            label6.Text = "Message";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 322);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(562, 110);
+            textBox4.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.MenuHighlight;
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(12, 457);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 34);
+            button1.TabIndex = 10;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            ClientSize = new Size(620, 512);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Contact Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
+        private TextBox textBox4;
+        private Button button1;
+    }
+}
