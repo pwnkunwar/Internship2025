fb094abf73f955380cbfdfd07f8dcdd115d15797
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-27 12:42:57 +0545

    Add new projects and update solution configuration
    Updated Internship2025.sln to include new projects ContactUsForm and EmergencyContactForm, and changed project type GUIDs for WinFromsApp and CalculatorApp. Added build configurations for new projects.
    
    Created EmergencyContactForm.csproj with .NET 8.0 Windows Forms settings. Added Form1.cs, Form1.Designer.cs, and Form1.resx for ContactUsForm. Added Program.cs to run Form1 from ContactUsForm namespace.

---
+++ EmergencyContactForm/Form1.Designer.cs
@@ -0,0 +1,430 @@
+﻿namespace ContactUsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            label9 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            label10 = new Label();
+            textBox5 = new TextBox();
+            label11 = new Label();
+            textBox6 = new TextBox();
+            label12 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            label13 = new Label();
+            label14 = new Label();
+            textBox9 = new TextBox();
+            label15 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(29, 157);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(234, 32);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(129, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(301, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Emergency Contact Form";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(159, 41);
+            label2.Name = "label2";
+            label2.Size = new Size(230, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Please fill out the form correclty";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(29, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(282, 157);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(234, 32);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(29, 205);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 17);
+            label4.TabIndex = 5;
+            label4.Text = "First Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(282, 205);
+            label5.Name = "label5";
+            label5.Size = new Size(70, 17);
+            label5.TabIndex = 6;
+            label5.Text = "Last Name";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(29, 246);
+            label6.Name = "label6";
+            label6.Size = new Size(54, 21);
+            label6.TabIndex = 7;
+            label6.Text = "E-mail";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(29, 279);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(234, 32);
+            textBox3.TabIndex = 8;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(282, 246);
+            label7.Name = "label7";
+            label7.Size = new Size(120, 21);
+            label7.TabIndex = 9;
+            label7.Text = "Phone Number";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(282, 279);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(234, 32);
+            textBox4.TabIndex = 10;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(29, 326);
+            label8.Name = "label8";
+            label8.Size = new Size(146, 17);
+            label8.TabIndex = 11;
+            label8.Text = "example@example.com";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(29, 365);
+            label9.Name = "label9";
+            label9.Size = new Size(190, 21);
+            label9.TabIndex = 12;
+            label9.Text = "Relationship with Patient";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(29, 401);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 21);
+            radioButton1.TabIndex = 13;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Parent";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(29, 437);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(62, 21);
+            radioButton2.TabIndex = 14;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Friend";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton3.Location = new Point(208, 401);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(55, 21);
+            radioButton3.TabIndex = 15;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Child";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton4.Location = new Point(367, 401);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(65, 21);
+            radioButton4.TabIndex = 16;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Sibling";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton5.Location = new Point(29, 473);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(59, 21);
+            radioButton5.TabIndex = 17;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Other";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(29, 518);
+            label10.Name = "label10";
+            label10.Size = new Size(70, 21);
+            label10.TabIndex = 18;
+            label10.Text = "Address";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(29, 554);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(465, 32);
+            textBox5.TabIndex = 19;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(29, 601);
+            label11.Name = "label11";
+            label11.Size = new Size(94, 17);
+            label11.TabIndex = 20;
+            label11.Text = "Street Address";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(29, 635);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(465, 32);
+            textBox6.TabIndex = 21;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.Location = new Point(29, 682);
+            label12.Name = "label12";
+            label12.Size = new Size(105, 17);
+            label12.TabIndex = 22;
+            label12.Text = "Street Address 2";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(29, 721);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(234, 32);
+            textBox7.TabIndex = 23;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(282, 721);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(234, 32);
+            textBox8.TabIndex = 24;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label13.Location = new Point(29, 767);
+            label13.Name = "label13";
+            label13.Size = new Size(29, 17);
+            label13.TabIndex = 25;
+            label13.Text = "City";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label14.Location = new Point(294, 767);
+            label14.Name = "label14";
+            label14.Size = new Size(106, 17);
+            label14.TabIndex = 26;
+            label14.Text = "State / Provience";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(29, 806);
+            textBox9.Multiline = true;
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(234, 32);
+            textBox9.TabIndex = 27;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label15.Location = new Point(29, 857);
+            label15.Name = "label15";
+            label15.Size = new Size(109, 17);
+            label15.TabIndex = 28;
+            label15.Text = "Postal / Zip Code";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.HotTrack;
+            button1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(196, 891);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 39);
+            button1.TabIndex = 29;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(541, 955);
+            Controls.Add(button1);
+            Controls.Add(label15);
+            Controls.Add(textBox9);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(label12);
+            Controls.Add(textBox6);
+            Controls.Add(label11);
+            Controls.Add(textBox5);
+            Controls.Add(label10);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(textBox4);
+            Controls.Add(label7);
+            Controls.Add(textBox3);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "EmergencyContactForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox3;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label8;
+        private Label label9;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private Label label10;
+        private TextBox textBox5;
+        private Label label11;
+        private TextBox textBox6;
+        private Label label12;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Label label13;
+        private Label label14;
+        private TextBox textBox9;
+        private Label label15;
+        private Button button1;
+    }
+}
