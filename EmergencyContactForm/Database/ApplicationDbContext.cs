002e77af1f1278367646537cb627443c2c803eb9
pwn1kunwar <pwn1kunwar@gmail.com>
2025-02-28 12:47:14 +0545

    Add EF Core setup and EmergencyContact model
    Updated EmergencyContactForm.csproj to include EntityFramework 6.5.1, Microsoft.EntityFrameworkCore.SqlServer 8.0.0, and Microsoft.EntityFrameworkCore.Tools 8.0.0. Added ApplicationDbContext class in ApplicationDbContext.cs to configure SQL Server database and manage EmergencyContact entities. Introduced EmergencyContact model in EmergencyContact.cs with validation annotations for various properties.

---
+++ EmergencyContactForm/Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmergencyContactForm.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmergencyContactForm.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<EmergencyContact> EmergencyContacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-3SS2B66;Initial Catalog=EmergencyContactForms;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        }
+
+    }
+}
---
+++ EmergencyContactForm/Models/EmergencyContact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyContactForm.Models
+{
+    public class EmergencyContact
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email  { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Relationship { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public string SecondAddress { get; set; }
+        [Required]
+        public string City { get; set; }    
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+    }
+}
