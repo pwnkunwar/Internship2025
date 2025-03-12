97ef90823f789108e2d2bbe94e42c889942ac1fd
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-12 17:26:18 +0545

    Product Class created

---
+++ ProductCRUD/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductCRUD.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid ProductId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1,1000)]
+        public decimal Price { get; set; }
+
+    }
+}
