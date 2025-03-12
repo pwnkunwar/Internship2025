794009c45c9121d0fe15268ad4e70e5f126c34a5
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-12 17:29:27 +0545

    Add new projects and classes for forms and models
    Updated Internship2025.sln to include RegisterForm, LoginForm,
    ForgotPasswordForm, and ProductCRUD projects. Added Data folder
    to ProductCRUD. Created Product class in ProductCRUD.Models
    with validation properties. Added Program class to
    RegisterAndLogin namespace to run frmRegister form. Created
    RegisterAndLogin.csproj targeting .NET 8.0 with nullable
    reference types and implicit usings. Added Form1 class,
    designer, and resource files to WinFormsApplication namespace.
    Created Program class in WinFormsApplication to run Form1 form.
    Created WinFormsApplication.csproj targeting .NET 8.0 with
    nullable reference types and implicit usings. Added Client
    class to WinFormsApplication.Models and ClientRepository class
    to WinFormsApplication.Repositories with GetClients method.
    
    Add new projects and classes for Register and Login forms
    
    Updated Internship2025.sln to include RegisterForm, LoginForm, ForgotPasswordForm, and ProductCRUD projects. Introduced RegisterAndLogin namespace in Program.cs with main entry point for frmRegister. Created RegisterAndLogin.csproj targeting .NET 8.0.
    
    Added WinFormsApplication project with Form1, Form1.Designer.cs, and Form1.resx. Updated Program.cs to run Form1. Created WinFormsApplication.csproj targeting .NET 8.0.
    
    Added Client class in WinFormsApplication.Models and ClientRepository class in WinFormsApplication.Repositories for database operations.

---
+++ WinFormsApplication/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApplication.Models
+{
+    public class Client
+    {
+        public int id;
+        public string firstName = "";
+        public string lastName = "";
+        public string email = "";
+        public string phone = "";
+        public string address = "";
+        public string createdAt = "";
+    }
+}
---
+++ WinFormsApplication/Repositories/ClientRepositry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApplication.Models;
+
+namespace WinFormsApplication.Repositories
+{
+    public class ClientRepositry
+    {
+        private readonly string connectionString = "Data Source=.;Initial Catalog=Client;Integrated Security=True;Trust Server Certificate=True";
+    }
+
+    public List<Client> GetClients()
+    {
+        var clients = new List<Client>();
+        try
+        {
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using(var command = new SqlCommand("SELECT * FROM Clients", connection))
+                {
+                    using(var reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            var client = new Client();
+                            client.id = reader.GetInt32(0);
+                            client.firstName = reader.GetString(1);
+                            client.lastName = reader.GetString(2);
+                            client.email = reader.GetString(3);
+                            client.phone = reader.GetString(4);
+                            client.address = reader.GetString(5);
+                            client.createdAt = reader.GetString(6);
+                            clients.Add(client);
+                        }
+                    }
+                }
+            }
+}
