73c73acd7594bca46f850dbf9b704f26e4350664
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-12 21:17:30 +0545

    Add DI for DbContext and async product creation method
    - Injected ApplicationDbContext into ProductController for DB access.
    - Added CreateAsync method to handle product creation via POST.
    - Included necessary namespaces for database context and models.

--- ProductCRUD/Controllers/ProductController.cs
+++ ProductCRUD/Controllers/ProductController.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductCRUD.Database;
+using ProductCRUD.Models;
 
 namespace ProductCRUD.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _db.Products.ToList();
+            return View(products);
         }
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Products.AddAsync(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
         }
     }
 }
