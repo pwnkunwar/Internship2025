7e631fd0ed0151c88a0bb3330f31a120d2125a55
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-16 23:47:33 +0545

    Add new UI components to Form1 and update layout
    Updated InitializeComponent in Form1.Designer.cs to add two DateTimePicker controls (dtpStartDate, dateTimePicker2) and six Button controls (btnThisMonth, btnToday, last7Days, this30Days, btnCustom, btnOK). Set properties for these controls, increased ClientSize from 800, 450 to 1401, 505, and added controls to the form's Controls collection. Declared private fields for each new control in Form1 class.

--- DataDashboardApp/Form1.Designer.cs
+++ DataDashboardApp/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dtpStartDate = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            btnThisMonth = new Button();
+            btnToday = new Button();
+            last7Days = new Button();
+            this30Days = new Button();
+            btnCustom = new Button();
+            btnOK = new Button();
             SuspendLayout();
+            // 
+            // dtpStartDate
+            // 
+            dtpStartDate.CustomFormat = "MMM dd, yyyy";
+            dtpStartDate.Format = DateTimePickerFormat.Custom;
+            dtpStartDate.Location = new Point(158, 12);
+            dtpStartDate.Name = "dtpStartDate";
+            dtpStartDate.Size = new Size(200, 23);
+            dtpStartDate.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CustomFormat = "MMM dd, yyyy";
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.Location = new Point(387, 12);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 1;
+            // 
+            // btnThisMonth
+            // 
+            btnThisMonth.Location = new Point(1269, 10);
+            btnThisMonth.Name = "btnThisMonth";
+            btnThisMonth.Size = new Size(120, 30);
+            btnThisMonth.TabIndex = 2;
+            btnThisMonth.Text = "This Month";
+            btnThisMonth.UseVisualStyleBackColor = true;
+            // 
+            // btnToday
+            // 
+            btnToday.Location = new Point(891, 10);
+            btnToday.Name = "btnToday";
+            btnToday.Size = new Size(120, 30);
+            btnToday.TabIndex = 3;
+            btnToday.Text = "Today";
+            btnToday.UseVisualStyleBackColor = true;
+            // 
+            // last7Days
+            // 
+            last7Days.Location = new Point(1017, 10);
+            last7Days.Name = "last7Days";
+            last7Days.Size = new Size(120, 30);
+            last7Days.TabIndex = 4;
+            last7Days.Text = "Last 7 Days";
+            last7Days.UseVisualStyleBackColor = true;
+            // 
+            // this30Days
+            // 
+            this30Days.Location = new Point(1143, 10);
+            this30Days.Name = "this30Days";
+            this30Days.Size = new Size(120, 30);
+            this30Days.TabIndex = 5;
+            this30Days.Text = "This 30 Days";
+            this30Days.UseVisualStyleBackColor = true;
+            // 
+            // btnCustom
+            // 
+            btnCustom.Location = new Point(765, 10);
+            btnCustom.Name = "btnCustom";
+            btnCustom.Size = new Size(120, 30);
+            btnCustom.TabIndex = 6;
+            btnCustom.Text = "Custom";
+            btnCustom.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new Point(719, 12);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(40, 30);
+            btnOK.TabIndex = 7;
+            btnOK.Text = "Ok";
+            btnOK.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1401, 505);
+            Controls.Add(btnOK);
+            Controls.Add(btnCustom);
+            Controls.Add(this30Days);
+            Controls.Add(last7Days);
+            Controls.Add(btnToday);
+            Controls.Add(btnThisMonth);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dtpStartDate);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -42,5 +130,14 @@
         }
 
         #endregion
+
+        private DateTimePicker dtpStartDate;
+        private DateTimePicker dateTimePicker2;
+        private Button btnThisMonth;
+        private Button btnToday;
+        private Button last7Days;
+        private Button this30Days;
+        private Button btnCustom;
+        private Button btnOK;
     }
 }
