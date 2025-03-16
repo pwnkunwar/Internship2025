6ad7f627a1123ed69007e3b17090319b1491b3db
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-16 23:20:14 +0545

    Refactor Dashboard class and add GetOrderAnalisys method
    Updated using directives in Dashboard.cs to include System.Globalization.
    Changed GrossRevenueList property type to List<RevenueByDate>.
    Added private method GetOrderAnalisys to initialize properties,
    execute SQL query, process results, group by date range, and
    populate GrossRevenueList with RevenueByDate instances.

--- DataDashboardApp/Models/Dashboard.cs
+++ DataDashboardApp/Models/Dashboard.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace DataDashboardApp.Models
         public int NumProducts { get; private set; }
         public List<KeyValuePair<string,int>> TopProductsList { get; private set; }
         public List<KeyValuePair<string, int>> UnderStockList { get; private set; }
-        public List<KeyValuePair<string, int>> GrossRevenueList { get; private set; }
+        public List<RevenueByDate> GrossRevenueList { get; private set; }
         public int NumOrders { get;  set; }
         public decimal TotalRevenue { get;  set; }
         public decimal TotalProfit { get;  set; }
@@ -62,6 +63,87 @@ namespace DataDashboardApp.Models
                     command.Parameters.AddWithValue("@startDate", System.Data.SqlDbType.DateTime).Value = startDate;
                     command.Parameters.AddWithValue("@endDate", System.Data.SqlDbType.DateTime).Value = endDate;
                     NumOrders = (int)command.ExecuteScalar();
+                }
+            }
+        }
+
+        private void GetOrderAnalisys()
+        {
+            GrossRevenueList = new List<RevenueByDate>();
+            TotalProfit = 0;
+
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = @"Select OrderDate, sum(TotalAmount)
+                                           from[Order]
+                                           where OrderDate between @fromDate and @toDate
+                                            group by OrderDate";
+                    command.Parameters.Add("@fromDate", System.Data.SqlDbType.DateTime).Value = startDate;
+                    command.Parameters.Add("@toDate", System.Data.SqlDbType.DateTime).Value = endDate;
+                    var reader = command.ExecuteReader();
+                    var resultTable = new List<KeyValuePair<DateTime, decimal>>();
+                    while (reader.Read())
+                    {
+                        resultTable.Add(
+                            new KeyValuePair<DateTime, decimal>((DateTime)reader[0], (decimal)reader[1])
+                            );
+                        TotalProfit = TotalRevenue * 0.2m;
+                    }
+                    TotalProfit = TotalRevenue * 0.2m;
+                    reader.Close();
+
+                    //Group by Days
+                    if (numberDays <= 30)
+                    {
+                        foreach (var item in resultTable)
+                        {
+                            GrossRevenueList.Add(new RevenueByDate
+                            {
+                                Date = item.Key.ToString("dd MMM"),
+                                TotalAmount = item.Value
+                            });
+                        }
+                    }
+                    // Group by Weeks
+                    else if(numberDays <= 92)
+                    {
+                        GrossRevenueList = (from orderList in resultTable
+                                            group orderList by CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(
+                                                orderList.Key, CalendarWeekRule.FirstDay, DayOfWeek.Monday)
+                                            into order
+                                            select new RevenueByDate
+                                            {
+                                                Date = "Week " + order.Key.ToString(),
+                                                TotalAmount = order.Sum(amount => amount.Value)
+                                            }).ToList();
+                    }
+                    else if (numberDays <= (365 * 2))
+                    {
+                        bool isYear = numberDays <= 365 ? true : false;
+                        GrossRevenueList = (from orderList in resultTable
+                                            group orderList by orderList.Key.ToString("MMM yyyy")
+                                            into order
+                                            select new RevenueByDate
+                                            {
+                                                Date = isYear ? order.Key.Substring(0,order.Key.IndexOf(" ")) : order.Key,
+                                                TotalAmount = order.Sum(amount => amount.Value)
+                                            }).ToList();
+                    }
+                    else 
+                    {
+                        GrossRevenueList = (from orderList in resultTable
+                                            group orderList by orderList.Key.ToString("MMM yyyy")
+                                            into order
+                                            select new RevenueByDate
+                                            {
+                                                Date = order.Key,
+                                                TotalAmount = order.Sum(amount => amount.Value)
+                                            }).ToList();
+                    }
                 }
             }
         }
