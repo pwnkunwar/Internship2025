e25a86f5b4ee2b29ecd05bb50eb06d9be6679476
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-20 12:37:33 +0545

    Add DemoController and update Index view for data demo
    Introduce DemoController in ProductCRUD.Controllers with an Index
    action method to demonstrate ViewBag, ViewData, and TempData usage.
    Update Index.cshtml to display data from these mechanisms, showing
    how they can be used in an ASP.NET Core MVC application.

---
+++ ProductCRUD/Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCRUD.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is ViewBag Data";
+
+            ViewData["Info"] = "This is ViewData Data";
+
+            TempData["Notice"] = "This is TempData Data (Survives Redirect)";
+            return View();
+        }
+
+    }
+}
