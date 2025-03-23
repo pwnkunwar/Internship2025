15d7da6b9d13a77cc8c2f3dd7422f8d696514da9
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-23 19:32:13 +0545

    Add SchoolProject with new Student model and dependencies
    Updated Internship2025.sln to include SchoolProject with nested sub-projects.
    Added GlobalSection for NestedProjects in solution file.
    Modified SchoolProject.Models.csproj to reference ServiceStack.Interfaces v8.6.0.
    Added Student class to SchoolProject.Models with various properties and unique KeyId.

---
+++ SchoolProject.Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }    
+        public DateTime DOB { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public bool Selected { get; set; }
+        [Unique]
+        public string KeyId { get; set; }
+    }
+}
