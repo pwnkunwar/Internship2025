64638fcebeef40e904553da6e579e890b267648e
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-23 20:23:21 +0545

    Update project to .NET 8 and add ApplicationDBContext
    Updated SchoolProject.Repositories.csproj to target .NET 8.0 and
    include Microsoft.AspNetCore.Identity.EntityFrameworkCore package
    version 8.0.14. Added project reference to SchoolProject.Models.
    Introduced ApplicationDBContext class in ApplicationDbContext.cs,
    inheriting from IdentityDbContext<ApplicationUser> for ASP.NET Core
    Identity with custom ApplicationUser class.

---
+++ SchoolProject.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using SchoolProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Repositories
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
