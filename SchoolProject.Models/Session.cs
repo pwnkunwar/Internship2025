f41b0cd472cfce16dd659f3fe6860b56b44c107f
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-23 21:43:40 +0545

    Add new properties and DbContext configurations
    Updated the `Session` class in `SchoolProject.Models` to include `Enrollment` and `TeacherSessions` properties. Added `Microsoft.EntityFrameworkCore` to `ApplicationDbContext.cs` and updated `ApplicationDBContext` with a constructor accepting `DbContextOptions<ApplicationDBContext>`. Added `DbSet` properties for managing various entities and configured relationships and foreign key constraints in `OnModelCreating`. Updated `SchoolProject.Web.csproj` to reference `SchoolProject.Models` and `SchoolProject.Repositories`.

--- SchoolProject.Models/Session.cs
+++ SchoolProject.Models/Session.cs
@@ -11,5 +11,7 @@ namespace SchoolProject.Models
         public int Id { get; set; }
         public string Start { get; set; }   
         public string End { get; set; }
+        public ICollection<Enroll> Enrollment { get;set; } = new HashSet<Enroll>();
+        public ICollection<TeacherSession> TeacherSessions { get; set; } = new HashSet<TeacherSession>();
     }
 }
--- SchoolProject.Repositories/ApplicationDbContext.cs
+++ SchoolProject.Repositories/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SchoolProject.Models;
 using System;
 using System.Collections.Generic;
@@ -10,5 +11,43 @@ namespace SchoolProject.Repositories
 {
     public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Session> YearlySessions { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Enroll> Enroll     { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AssignGrade>().HasOne(x=>x.Grade).
+                WithMany(z=>z.AssignGrade).HasForeignKey(x=>x.GradeId).OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<AssignGrade>().HasOne(x=>x.Teacher).
+                WithMany(z=>z.AssignGrades).HasForeignKey(x=>x.TeacherId).OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<Enroll>().HasOne(x=>x.Student).
+                WithMany(z => z.YearlySession).HasForeignKey(x => x.StudentId).OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<Enroll>().HasOne(x=>x.Session).
+                WithMany(z=>z.Enrollment).HasForeignKey(x=>x.GradeId).OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<Enroll>().HasOne(x=>x.Grade).
+                WithMany(z=>z.Enrolls).HasForeignKey(x=>x.GradeId).OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<TeacherSession>().HasOne(x=>x.Teacher).
+                WithMany(z=>z.TeacherSessions).HasForeignKey(x=>x.TeacherId).OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<TeacherSession>().HasOne(x=>x.Session).
+                WithMany(z=>z.TeacherSessions).HasForeignKey(x=>x.SessionId).OnDelete(DeleteBehavior.SetNull);
+
+            base.OnModelCreating(builder);
+
+        }
     }
 }
