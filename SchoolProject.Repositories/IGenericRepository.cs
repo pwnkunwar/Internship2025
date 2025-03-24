cc0d819e3f8736b2d2e0f2b288da881f3636395e
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-24 23:16:22 +0545

    Add IGenericRepository<T> interface for generic data access
    Introduce IGenericRepository<T> in SchoolProject.Repositories
    namespace to provide a generic repository pattern. This interface
    supports synchronous and asynchronous operations for retrieving,
    adding, updating, and deleting entities. It also includes methods
    for filtering, ordering, and including related entities in queries,
    and extends IDisposable for resource management.

---
+++ SchoolProject.Repositories/IGenericRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Repositories
+{
+    public interface  IGenericRepository<T> : IDisposable where T : class
+    {
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+            bool disabledTracking = true);
+
+        T GetById(object id);
+        T GetByIdAsync(Expression<Func<T,bool>> filter = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+            bool disabledTracking = true);
+
+        void Add(T entity); 
+        Task<T> AddAsync(T entity);
+        void AddRange(List<T> entity);
+        void Update(T entity);  
+        Task<T> UpdateAsync(T entity);
+        void Delete(T entity);
+        Task<T> DeleteAsync(List<T> entityList);
+        bool Exists(Expression<Func<T, bool>> filter = null);
+
+    }
+}
