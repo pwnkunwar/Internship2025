d4ced4775054c1e64ef2f84204f2880f1129b81f
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-25 20:03:22 +0545

    Add DbInitializer class and IDbInitializer interface
    Removed Class1 from SchoolProject.Utilities.
    Added DbInitializer class implementing IDbInitializer with a UserManager field.
    Added IDbInitializer interface with an Initialize method.

--- SchoolProject.Utilities/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SchoolProject.Utilities
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ SchoolProject.Utilities/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManger<APplicationUser> _userManager;
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
