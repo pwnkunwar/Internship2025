42fdcb478446f8e37c84a2228753aaf90c2e48e4
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-25 20:11:59 +0545

    Add WebsiteRole class with role constants
    Introduce WebsiteRole.cs in SchoolProject.Utilities namespace.
    Include using directives for common namespaces.
    Add static class WebsiteRole with constants for website roles.

---
+++ SchoolProject.Utilities/WebsiteRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Utilities
+{
+    public static class WebsiteRole
+    {
+        public const string Website_Admin = "Admin";
+        public const string Website_Teacher = "Teacher";
+        public const string Website_Student = "Student";
+        public const string Website_Employee = "Employee";  
+    }
+}
